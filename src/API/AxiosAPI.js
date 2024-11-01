a8c3a9d2ecdb39636317d4eaa7b1aa85f98fd003
soomin <tnals2832@naver.com>
2024-11-01 19:15:07 +0900

    add:assignment5

--- src/API/AxiosAPI.js
+++ src/API/AxiosAPI.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
-const server = process.env.REACT_APP_API_URL;
+//경로 지정
+const server = "http://ec2-13-209-3-68.ap-northeast-2.compute.amazonaws.com:8080";
 
 // 여기서 중요한 점은 서버의 주소를 어떻게 완성하느냐입니다. 스웨거를 잘 확인하고 서버에게 어떻게 주소를 보낼 수 있을 지 생각해보세요.
 // 템플릿 리터럴 (``) 을 이용해서 표현식을 잘 다룰 수 있도록 해봅시다.
 
 export const getMembersAPI = async (part) => {
     try {
-        //your code here
-
+        // axios 사용해서 '/user.json' 엔드 포인드에서 데이터 가져옴
+        // parameter: part
+        const response = await axios.get(`${server}/user?part=${part}`, part);
+        return response.data;
     } catch (error) {
         console.error("Error fetching(part) user data:", error);
         throw error;
@@ -18,8 +21,9 @@ export const getMembersAPI = async (part) => {
 
 export const postMemberAPI = async (data) => {
     try {
-        //your code here
-
+        // axois 사용해 추가하려는 데이터 담아 서버로 보냄
+        const response = await axios.post(`${server}/user`, data);
+        return response;
     } catch (error) {
         console.error("Error updating user data:", error);
         throw error;
